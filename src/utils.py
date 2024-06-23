4f9e4d75a76cd63797b7a49af9f8e0689b860cf3
arthurkunzler <72428522+arthurkunzler@users.noreply.github.com>
2024-06-23 19:10:31 +0000

    add documentacao incial do que vai precisar ser feito pra limpeza da base de dados

--- src/utils.py
+++ src/utils.py
@@ -28,7 +28,7 @@ f_columns={"DT_SAIDA": "Data_saida", "DT_INTER" : "Data_internacao", "DIAS_PERM"
 # Limpeza dos dados:
 # 1. Retirar colunas zeradas
 # 2. Renomear as colunas importantes
-# 3. Retirar linhas onde alguma das colunas importantes forem zeradas
+# 3. Retirar linhas onde o valor de alguma das colunas importantes forem zeradas
 # 4. Retirar linhas com data saida ou data internacao diferentes do ano em questao (ex.: dados de 2015, porem tem linhas com datas em ano 2014)
 # 5. Adicionar uma coluna com o ano, para permitir agrupamento mais facil no PowerBI
 # 6. Transformar idade: ou concatenar numero + label (ex 3 anos, 2 dias) ou padronizar (transformar tudo para a mesma unidade de medida)
